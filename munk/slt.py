663fc1716c45895b17431b39d48fdfc98a3c6121
Julien de Carufel <julien.decarufel@gmail.com>
2018-11-23 19:48:18 -0500

    Fix Select

--- munk/slt.py
+++ munk/slt.py
@@ -5,4 +5,4 @@ def slt(model_name, conditions, file_name='db.json'):
     db = json.load(open(file_name, 'r'))
     if any(condition not in db['models'][model_name] for condition in conditions):
         raise KeyError
-    return [db['tables'][model_name][model] for model in db['tables'][model_name] if all(model['condition'] == value for condition, value in conditions)]
+    return [model for id, model in db['tables'][model_name].items() if all(model[condition] == value for condition, value in conditions.items())]
