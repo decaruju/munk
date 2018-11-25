fc4c16e9bf13962bb346cfeffc6965325f1eb0f1
Julien de Carufel <julien.decarufel@gmail.com>
2018-11-25 14:33:40 -0500

    mnk-1 Remove unnecessary .read()

--- munk/seed.py
+++ munk/seed.py
@@ -4,7 +4,7 @@ from db import DB
 
 def seed_db(seed_file_name, db_file_name):
     with open(seed_file_name, 'r') as file:
-        seed = yaml.load(file.read())
+        seed = yaml.load(file)
 
 
     db = DB(db_file_name)
