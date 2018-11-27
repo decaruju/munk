0a464d3dcad54f763b0cc69bfebd624fb37b8698
decaruju <julien.decarufel@gmail.com>
2018-11-26 21:12:31 -0500

    mnk-2 Add indexes to fields (#12)
    * mnk-2 Add index on insert and select
    
    * mnk-2 Add index to update and delete

--- munk/initializer.py
+++ munk/initializer.py
@@ -3,15 +3,17 @@ import json
 from model import Model
 
 
+
 with open('examples/db.yaml') as file:
     data = yaml.load(file.read())
-
-
+    
+    
+    
 models = {model: Model(fields).dict() for model, fields in data.items()}
 
-
 json.dump({
-    'models': models,
-    'tables': {model: {} for model in models},
-    'sequences': {model: 0 for model in models}
-}, open('examples/db.json', 'w'))
+        'models': models,
+        'tables': {model: {} for model in models},
+        'sequences': {model: 0 for model in models},
+        'indexes': {model_name: {field: {} for field, attrs in model.items() if attrs['index']} for model_name, model in models.items()},
+}, open('examples/db.yaml', 'w'))
--- munk/seed.py
+++ munk/seed.py
@@ -2,10 +2,9 @@ import yaml
 from db import DB
 
 
-def seed_db(seed_file_name, db_file_name):
+def seed_db(db_file_name, seed_file_name):
     with open(seed_file_name, 'r') as file:
-        seed = yaml.load(file)
-
+        seed = yaml.load(file.read())
 
     db = DB(db_file_name)
 
