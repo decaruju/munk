5efb3703176b112f2a53279d6494f7abd12afe0e
Julien de Carufel <julien.decarufel@gmail.com>
2018-12-08 09:37:24 -0500

    Init django app

--- munk/db.py
+++ munk/db.py
@@ -23,19 +23,43 @@ class DB:
         with open(self.file_name, 'w') as file:
             json.dump(db, file)
 
-    def insert(self, model_name, model):
-        db = self.db
-        if not set(model) <= set(db['models'][model_name]):
-            raise ValueError(f'{model} and {db["models"][model_name]}')
-        for field, value in model.items():
-            model[field] = TYPES[db['models'][model_name][field]['type']](value)
+    def check_fields(self, model_name, model):
+        model_fields = self.db['models'][model_name]
+        for field in model:
+            if field not in model_fields:
+                raise ValueError(f'{field} not in {model_fields}')
+        for field, params in model_fields.items():
+            if params['required'] and field not in model:
+                raise ValueError(f'{field} is required for model {model_name}')
 
-        db['tables'][model_name][db['sequences'][model_name]] = model
+    def increment_sequence(self, model_name, total=1):
+        db = self.db
+        db['sequences'][model_name] += 1
+        self.db = db
+
+    def add_indexes(self, model_name, model):
+        db = self.db
         for field, value in model.items():
             if db['models'][model_name][field]['index']:
                 db['indexes'][model_name][field].setdefault(value, []).append(str(db['sequences'][model_name]))
-        db['sequences'][model_name] += 1
         self.db = db
+
+    def cast_fields(self, model_name, model):
+        for field, value in model.items():
+            model[field] = TYPES[db['models'][model_name][field]['type']](value)
+        return model
+
+    def insert(self, model_name, model):
+        self.check_fields(model_name, model)
+        model = self.cast_fields(model_name, model)
+
+        db = self.db
+        db['tables'][model_name][db['sequences'][model_name]] = model
+        self.db = db
+
+        self.add_indexes(model_name, model)
+        self.increment_sequence(model_name)
+
 
     def select(self, model_name, conditions={}):
         db = self.db
--- munk/field.py
+++ munk/field.py
@@ -3,11 +3,12 @@ class Field:
         self.type = kwargs['type']
         self.index = kwargs.get('index', False)
         self.to = kwargs.get('to', None)
+        self.required = kwargs.get('required', False)
 
     def __str__(self):
         return str(self.type)
 
     def dict(self):
-        return {'type': self.type, 'index': self.index, 'to': self.to}
+        return {'type': self.type, 'index': self.index, 'to': self.to, 'required': self.required}
 
     __repr__ = __str__
